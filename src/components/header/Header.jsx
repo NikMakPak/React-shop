9eda18168e9a2c87eb8071c393abdcc28fc5e3bf
nikmakpak <nicement28@gmail.com>
2022-09-21 09:42:10 +0300

    fix: add styles and like icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Btn from '../button/Btn'
 import css from './Header.module.css'
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <header className={css.header}>
       <h1>R-STORE</h1>
-      <Btn>Войти</Btn>
+      <Link className={'link'} to={'/login'}>
+        Войти
+      </Link>
     </header>
   )
 }
