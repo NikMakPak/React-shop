df161bae04a4a4e62b48ee78b3388c28ffd63736
nikmakpak <nicement28@gmail.com>
2022-09-21 13:52:54 +0300

    feat: add adaptive to main page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,9 @@ import css from './Header.module.css'
 const Header = ({ jsxElem, fontPx }) => {
   return (
     <header className={css.header}>
-      <h1 style={{ fontSize: fontPx }}>R-STORE</h1>
+      <h1 className={'header__title'} style={{ fontSize: fontPx }}>
+        R-STORE
+      </h1>
       {jsxElem}
     </header>
   )
